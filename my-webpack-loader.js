4601a51c16f5fe893a5760b613844019ad955e14
minsun <yamyam@pentabreed.com>
2020-11-22 22:33:02 +0900

    webpack-loader 작성 중

---
+++ my-webpack-loader.js
@@ -0,0 +1,4 @@
+module.exports = function myWebPackLoader (content) {
+  console.log('myWebPackLoader가 동작함');
+  return content;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,15 @@ module.exports = {
   output: {
     filename: "[name].js",
     path: path.resolve("./dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // 모든 js를 선택 
+        use: [
+          path.resolve("./my-webpack-loader.js")
+        ]
+      }
+    ]
   }
 }
